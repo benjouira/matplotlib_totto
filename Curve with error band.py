75941d674b72b8daccf43d5fa5af70ab9ef170b8
Rabii <rabi3jouira@gmail.com>
2022-01-20 18:48:42 +0100

    Update Curve with error band.py

--- Curve with error band.py
+++ Curve with error band.py
@@ -12,3 +12,45 @@ x, y = r * np.cos(t), r * np.sin(t)
 fig, ax = plt.subplots()
 ax.plot(x, y, "k")
 ax.set(aspect=1)
+
+
+
+# ////////////////////////////////////////
+
+def draw_error_band(ax, x, y, err, **kwargs):
+    # Calculate normals via centered finite differences (except the first point
+    # which uses a forward difference and the last point which uses a backward
+    # difference).
+    dx = np.concatenate([[x[1] - x[0]], x[2:] - x[:-2], [x[-1] - x[-2]]])
+    dy = np.concatenate([[y[1] - y[0]], y[2:] - y[:-2], [y[-1] - y[-2]]])
+    l = np.hypot(dx, dy)
+    nx = dy / l
+    ny = -dx / l
+
+    # end points of errors
+    xp = x + nx * err
+    yp = y + ny * err
+    xn = x - nx * err
+    yn = y - ny * err
+
+    vertices = np.block([[xp, xn[::-1]],
+                         [yp, yn[::-1]]]).T
+    codes = np.full(len(vertices), Path.LINETO)
+    codes[0] = codes[len(xp)] = Path.MOVETO
+    path = Path(vertices, codes)
+    ax.add_patch(PathPatch(path, **kwargs))
+
+
+axs = (plt.figure(constrained_layout=True)
+       .subplots(1, 2, sharex=True, sharey=True))
+errs = [
+    (axs[0], "constant error", 0.05),
+    (axs[1], "variable error", 0.05 * np.sin(2 * t) ** 2 + 0.04),
+]
+for i, (ax, title, err) in enumerate(errs):
+    ax.set(title=title, aspect=1, xticks=[], yticks=[])
+    ax.plot(x, y, "k")
+    draw_error_band(ax, x, y, err=err,
+                    facecolor=f"C{i}", edgecolor="none", alpha=.3)
+
+plt.show()
